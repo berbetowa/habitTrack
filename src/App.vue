abf5a9e784b08266d10558331a285f0816350838
berbetowa <natasha.berbetova@gmail.com>
2024-09-29 21:54:48 +0200

    nav bar changed

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,38 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import {RouterLink, RouterView} from 'vue-router'
+
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
+      <img alt="Vue logo" class="logo" src="@/assets/images/logo.jpeg" width="125" height="125"/>
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <!--        <RouterLink to="/">Home</RouterLink>-->
+        <!--        <RouterLink to="/about">About</RouterLink>-->
+        <RouterLink to="/">Dashboard</RouterLink>
+        <RouterLink to="/addhabit">Add Habit</RouterLink>
       </nav>
     </div>
   </header>
 
-  <RouterView />
+  <RouterView/>
 </template>
 
 <style scoped>
 header {
+  background: var(--color-background-nav);
   line-height: 1.5;
   max-height: 100vh;
+  padding: 0 1.5rem;
 }
 
 .logo {
   display: block;
-  margin: 0 auto 2rem;
+  border-radius: 15px;
+  width: 5rem;
+  height: 5rem;
+  margin: 0.5rem auto 0 auto;
 }
 
 nav {
@@ -38,48 +42,27 @@ nav {
   margin-top: 2rem;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
 nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+  display: block;
+  color: var(--color-text);
+  background: var(--color-background-soft);
+  border-radius: 10px;
+  padding: 0.5rem;
+  width: 100%;
+  margin: 0.7rem;
 }
+
+nav a.router-link-exact-active {
+  color: var(--hbt-white);
+}
+
+nav a:hover {
+  background: var(--color-text);
+  color: var(--color-background-soft);
+}
+
 
 nav a:first-of-type {
   border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
 }
 </style>
